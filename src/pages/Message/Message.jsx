f229690c2bbed2210cfa109d5be67a02a6f36081
Yunus Çetin <93588469+lordcetin@users.noreply.github.com>
2025-05-27 00:39:52 +0300

    af

--- src/pages/Message/Message.jsx
+++ src/pages/Message/Message.jsx
@@ -298,7 +298,7 @@ const Message = () => {
   }, []);
 
   const { messages, status, sendMessage } = useWebSocket(
-    `wss://borvey-backend-a8f223c668a5.herokuapp.com/connect?userId=${userInfo._id}`
+    `wss://borvey-backend-13d056a7e6c7.herokuapp.com/connect?userId=${userInfo._id}`
   );
 
   // const { messages, status, sendMessage } = useWebSocket(
--- src/utils/AxiosRequest.js
+++ src/utils/AxiosRequest.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { ApiRoutes } from "./ApiRoutes";
 // export const BASE_URL = "http://localhost:5050";
-const BASE_URL = "https://borvey-backend-a8f223c668a5.herokuapp.com/";
+const BASE_URL = "https://borvey-backend-13d056a7e6c7.herokuapp.com/";
 
 
 export const AxiosRequest = (method, url, data) => {
